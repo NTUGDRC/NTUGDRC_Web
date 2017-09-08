3ee16954f0a90737f7fe8c10e869b6890147c444
WooLNinesun <WooLNinesun@gmail.com>
2017-09-08 15:19:04 +0800

    init master repo

---
+++ src/index.js
@@ -0,0 +1,2 @@
+/* global DPLAYER_VERSION GIT_HASH */
+console.log(`${'\n'} %c NTUGDRC_Web ${NTUGDRC_Web_VERSION} ${GIT_HASH} ${'\n'}${'\n'}`, 'color: #fadfa3; background: #030307; padding:5px 0;');
